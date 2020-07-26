377e3b11887e51d8a85a48b57909b801fba5aea9
masao <Masao-Someki>
2020-07-27 00:24:31 +0900

    Add test script

---
+++ test/test_conformer.py
@@ -0,0 +1,15 @@
+# This is a test script
+
+import torch
+import json
+from CF import get_conformer
+
+def test_conformer():
+    conf = json.load(open('conformer.conf'))
+    net = get_conformer(**conf)
+    net.eval()
+    data = torch.randn(1, 32, conf['d_model'])
+    # data should be formatted as (B, L, D)
+    # B as batch-size, L as sequence-length, D as feature-dimension.
+    out = net(data)
+    assert out.shape == (1, 32, conf['d_model'])
